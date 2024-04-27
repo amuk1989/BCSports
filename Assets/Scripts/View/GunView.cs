1528db08d9ee8d2111163f8cb40a878f8c042eec
Andrey Muholzoev <muholzoev@gmail.com>
2024-04-27 21:43:36 +0700

    Update GunView.cs

--- Assets/Scripts/View/GunView.cs
+++ Assets/Scripts/View/GunView.cs
@@ -8,12 +8,13 @@ namespace View
     public class GunView : NetworkBehaviour
     {
         [SerializeField] private NetworkCharacterController _cc;
-        
+
         public override void FixedUpdateNetwork()
         {
             if (GetInput(out NetworkInputData data))
             {
                 Debug.Log(data.Direction);
+                transform.position += (Vector3)data.Direction * 0.01f;
             }
         }
     }
